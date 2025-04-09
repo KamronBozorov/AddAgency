c6d53953eaf9adb499f1d1117147ed72b169b4c4
KamronBozorov <bozorovkamron01@gmail.com>
2025-04-09 17:02:06 +0500

    second commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const sequelize = require("./config/db.js");
 const mainRoute = require("./routes/index.js");
 const cookieParser = require("cookie-parser");
 const errorHandler = require("./helpers/error.handler.js");
+const logger = require("./config/logger.js");
 
 const app = express();
 const PORT = config.get("port");
@@ -19,7 +20,7 @@ async function start() {
     await sequelize.sync({ alter: true });
     console.log("Connection has been established successfully.");
     app.listen(PORT, () => {
-      console.log(`Server is running on PORT-${PORT}`);
+      logger.info(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Unable to connect to the database:", error);
